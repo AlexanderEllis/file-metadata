7adc6d7d1b501b82dace0f4d46384f4cd0a18978
Alexander Ellis <ellisonjtk@gmail.com>
2016-05-20 22:19:17 -0400

    upload up to date

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var multer = require('multer');
+var upload = multer();
 
-app.get('*', function(req, res) {
-	res.end('Hello World!');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index.html');
 });
+
+app.post('/uploads/', upload.single('the-file'), function(req, res, next) {
+	res.end("Your file's size is " + req.file.size + " bytes.");
+});
+
 
 var port = process.env.PORT || 3000;
 
